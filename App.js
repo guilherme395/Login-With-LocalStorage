c989b604c9511775eb3bc0a579fad2fc5fb2eaa9
Guilherme Lima <guilhermelimaleite06@gmail.com>
2022-10-12 22:24:44 -0300

    Melhorado a validação...

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ if (UserLogin) {
     UserLogin.addEventListener("submit", (e) => {
         e.preventDefault();
         if (document.getElementById("Nome").value && document.getElementById("Senha").value) {
-            MsgAlert.innerHTML = `  <div class='alert alert-success' role='alert'>
-                                        Usuario Cadastrado com sucesso !!!
-                                    </div>`;
             setTimeout(() => {
-                window.location.reload(1);
-            }, "3000")
+                MsgAlert.innerHTML = `  <div class='alert alert-success' role='alert'>
+                                            Usuario Cadastrado com sucesso !!!
+                                        </div>`;
+                setTimeout(() => {
+                    window.location.reload(1);
+                }, "3000")
+            }, "1 000")
         } else {
             MsgAlert.innerHTML = `  <div class='alert alert-danger' role='alert'>
                                         Necessario Preencher todos os campos !!!
