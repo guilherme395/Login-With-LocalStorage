805f9965d3729f42569abb6e2ed56a9b6e5b2fa2
Guilherme Lima <guilhermelimaleite06@gmail.com>
2022-10-12 21:05:19 -0300

    Feito Algumas Validações (ESTATICAS).

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 const UserLogin = document.getElementById("Form-User-Login");
+const MsgAlert = document.getElementById("MsgAlert");
 
 if (UserLogin) {
     UserLogin.addEventListener("submit", (e) => {
         e.preventDefault();
-        if (document.getElementById("Nome").value && document.getElementById("Senha").value ) {
-            console.log("Por enquanto ta tudo certo meu brother !!!!");
-        }else{
-            alert("Por Favor Preencher todos os capos !!!");
+        if (document.getElementById("Nome").value && document.getElementById("Senha").value) {
+            MsgAlert.innerHTML = `  <div class='alert alert-success' role='alert'>
+                                        Usuario Cadastrado com sucesso !!!
+                                    </div>`;
+            setTimeout(() => {
+                window.location.reload(1);
+            }, "3000")
+        } else {
+            MsgAlert.innerHTML = `  <div class='alert alert-danger' role='alert'>
+                                        Necessario Preencher todos os campos !!!
+                                    </div>`;
         }
     });
 };
